3c666a5bb442c8df31c71ad224dd51c7e87e4332
NishiOwO <nishi@nishi.boats>
2026-02-15 19:59:51 +0900

    move to draw_sprite

--- lib/render.c
+++ lib/render.c
@@ -1,5 +1,37 @@
 #include <ze_runtime.h>
 
+static void draw_sprite(ze_runtime_t* rt, ze_sprite_t* spr) {
+	double cx = spr->target->costumes[spr->costume]->center_x;
+	double cy = spr->target->costumes[spr->costume]->center_y;
+	double w  = spr->target->costumes[spr->costume]->width;
+	double h  = spr->target->costumes[spr->costume]->height;
+
+	cx /= spr->target->costumes[spr->costume]->resolution;
+	cy /= spr->target->costumes[spr->costume]->resolution;
+	w /= spr->target->costumes[spr->costume]->resolution;
+	h /= spr->target->costumes[spr->costume]->resolution;
+
+	glBindTexture(GL_TEXTURE_2D, spr->target->costumes[spr->costume]->texture->id);
+	glColor3f(1, 1, 1);
+	glPushMatrix();
+	glTranslatef(spr->x, spr->y, 0);
+	glRotatef(-spr->angle, 0, 0, 1);
+	glBegin(GL_QUADS);
+	glTexCoord2f(0, 0);
+	glVertex2f(-cx, cy);
+
+	glTexCoord2f(0, 1);
+	glVertex2f(-cx, cy - h);
+
+	glTexCoord2f(1, 1);
+	glVertex2f(-cx + w, cy - h);
+
+	glTexCoord2f(1, 0);
+	glVertex2f(-cx + w, cy);
+	glEnd();
+	glPopMatrix();
+}
+
 void ze_render(ze_runtime_t* rt) {
 	int i;
 	glfwMakeContextCurrent(rt->window);
@@ -9,36 +41,10 @@ void ze_render(ze_runtime_t* rt) {
 
 	glEnable(GL_TEXTURE_2D);
 	for(i = 0; i < arrlen(rt->sprites); i++) {
-		ze_sprite_t* spr = rt->sprites[i];
-		double	     cx	 = spr->target->costumes[spr->costume]->center_x;
-		double	     cy	 = spr->target->costumes[spr->costume]->center_y;
-		double	     w	 = spr->target->costumes[spr->costume]->width;
-		double	     h	 = spr->target->costumes[spr->costume]->height;
-
-		cx /= spr->target->costumes[spr->costume]->resolution;
-		cy /= spr->target->costumes[spr->costume]->resolution;
-		w /= spr->target->costumes[spr->costume]->resolution;
-		h /= spr->target->costumes[spr->costume]->resolution;
-
-		glBindTexture(GL_TEXTURE_2D, spr->target->costumes[spr->costume]->texture->id);
-		glColor3f(1, 1, 1);
-		glPushMatrix();
-		glTranslatef(spr->x, spr->y, 0);
-		glRotatef(-spr->angle, 0, 0, 1);
-		glBegin(GL_QUADS);
-		glTexCoord2f(0, 0);
-		glVertex2f(-cx, cy);
-
-		glTexCoord2f(0, 1);
-		glVertex2f(-cx, cy - h);
-
-		glTexCoord2f(1, 1);
-		glVertex2f(-cx + w, cy - h);
-
-		glTexCoord2f(1, 0);
-		glVertex2f(-cx + w, cy);
-		glEnd();
-		glPopMatrix();
+		if(rt->sprites[i]->target->stage) draw_sprite(rt, rt->sprites[i]);
+	}
+	for(i = 0; i < arrlen(rt->sprites); i++) {
+		if(!rt->sprites[i]->target->stage) draw_sprite(rt, rt->sprites[i]);
 	}
 	glDisable(GL_TEXTURE_2D);
 
