6b53763d44c02156525f64e447f2c908e4887086
NishiOwO <nishi@nishi.boats>
2026-02-19 02:08:57 +0900

    looks_nextcostume

--- lib/block/looks.c
+++ lib/block/looks.c
@@ -22,7 +22,15 @@ static int looks_think(ba_thread_t* thread) {
 	return ba_status_next;
 }
 
+static int looks_nextcostume(ba_thread_t* thread) {
+	thread->sprite->costume++;
+	thread->sprite->costume = thread->sprite->costume % arrlen(thread->sprite->target->costumes);
+
+	return ba_status_next;
+}
+
 void ba_block_looks(ba_runtime_t* rt) {
 	ba_runtime_block_handler(rt, "looks_say", looks_say);
 	ba_runtime_block_handler(rt, "looks_think", looks_think);
+	ba_runtime_block_handler(rt, "looks_nextcostume", looks_nextcostume);
 }
